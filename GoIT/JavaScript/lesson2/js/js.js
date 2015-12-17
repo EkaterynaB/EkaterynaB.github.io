a7924d21374f01fc08f3cecf00bb227fee168bf5
EkaterynaB <kattyB2701@gmail.com>
2015-12-17 10:21:27 +0200

    Lesson 2

--- GoIT/JavaScript/lesson2/js/js.js
+++ GoIT/JavaScript/lesson2/js/js.js
@@ -1,31 +1,57 @@
 var arr = [];
 var userName = "";
 var flag = false;
+var newArr = [];
 
-for (var i = 0; i < 5; i ++) {
-    arr[i] = prompt("Введите имя пользователя").toLowerCase();
+for (var i = 0; i < 5; i++) {
+    arr[i] = prompt("Введите имя пользователя");
 
-    if (arr[i] == "" || arr[i] == " ") {
+    if (arr[i] == '' || arr[i] == ' ') {
         alert("Введите имя пользователя еще раз");
-        i -= i;
+        i--;
+    } else if (arr[i] == null) {
+        i = 5;
     }
+    
 }
 
-userName = prompt("Введите свое имя").toLowerCase();
+if (arr.length > 1) {
 
-while (userName == "") {
-    userName = prompt("Введите свое имя").toLowerCase();
-}
+    userName = prompt("Введите свое имя");
 
-for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === userName) {
-        flag = true;
-        break;
+    while (userName == "") {
+        userName = prompt("Введите свое имя");
     }
+
+    for (var i = 0; i < arr.length; i++) {
+        newArr[i] = arr[i].toLowerCase();
+    }
+
 }
 
-var newName = userName[0].toUpperCase() + userName.substr(1);
-console.log(newName);
+if (userName  != null) {
+
+    userName = userName.toLowerCase();
+
+    for (var i = 0; i < newArr.length; i++) {
+
+        if (newArr[i] === userName) {
+            flag = true;
+            break;
+        }
+
+    }
+
+}
+
+function ucFirst(str) {
+
+    if (!str) return str;
+
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+var newName = ucFirst(userName);
 
 if (flag) {
     alert(newName + ", вы успешно вошли!");
