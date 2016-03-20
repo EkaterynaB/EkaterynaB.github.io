de2580a79c91c4232a2ed58db30f644faf18720b
EkaterynaB <kattyB2701@gmail.com>
2016-03-20 18:46:36 +0200

    exam

--- GoIT/JavaScript/exam/js/js.js
+++ GoIT/JavaScript/exam/js/js.js
@@ -1,54 +1,58 @@
 $(function() {
 
-    $.ajax({
-                url: 'http://api.pixplorer.co.uk/image?word=snow&amount=7&size=l',
-                type: "GET",
-				dataType: "json",
-				cache: false,
-                success: function(data){
-                    console.log(data);
-                    // var list = data.images.map(function (val) {
-                    //   return "<div class='grid-item'><img src='"+val.imageurl+"'><span>"+val.word+"</span></div>";
-                    // }).join('');
+    function getImagesByRequest(phrase) {
 
+        $.ajax({
+            url: 'http://api.pixplorer.co.uk/image?word='+ phrase +'&amount=7&size=l',
+            type: "GET",
+            dataType: "json",
+            cache: false,
+            success: function(data){
+                $(".grid").empty();
+                var piclist = tmpl($('#activity__template').html(), {data: data});
+                $('.activity__wrapper').append(piclist);
 
-                    console.log(data);
-                    var piclist = tmpl($('#activity__template').html(), {data: data});
-                    $('.activity__wrapper').append(piclist);
+                $('.grid').isotope({
+                    itemSelector: '.grid-item',
+                    layoutMode: 'packery',
+                    packery: {
+                        gutter: 20
+                    }
+                });
 
+            }
+        });
 
-                    // $(".grid").append(list);
-                    $('.grid').isotope({
-                        itemSelector: '.grid-item',
-                        layoutMode: 'packery',
-                        packery: {
-                            gutter: 20
-                        }
-                    });
-                    // $('.grid').isotope({
-					// 	itemSelector: '.grid-item',
-					// 	layoutMode: 'masonry',
-					// 	masonry: {
-					// 		gutter: 20
-					// 	}
-					// });
-                }
-            });
+    }
 
+    getImagesByRequest('snowboarding');
+    var buttonSearch = $('.search__button');
 
+    $('.search__button').click(function(e) {
+        e.preventDefault();
+        var searchField = $(".input__form").val();
+        getImagesByRequest(searchField);
+})
 
             $('.jcarousel').jcarousel({
     			animation: 'slow',
     			wrap: 'circular'
     		})
 
-		$('.jcarousel-prev')
-            .jcarouselControl({
+		    $('.jcarousel-prev')
+                .jcarouselControl({
 				target: '-=1'
 			});
 
-		$('.jcarousel-next')
-            .jcarouselControl({
+		    $('.jcarousel-next')
+                .jcarouselControl({
 				target: '+=1'
 			});
+
+            function init() {
+                getImagesByRequest();
+                formActivity.addEventListener('submit', getUserQuery);
+            }
+
+            document.addEventListener('DOMContentLoaded', init);
 })
