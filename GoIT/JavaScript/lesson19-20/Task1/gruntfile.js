32302d4d5f8019a99443f5dd9fe94678b874e4aa
EkaterynaB <kattyb2701@gmail.com>
2016-03-31 02:35:53 +0300

    lesson19-20

--- GoIT/JavaScript/lesson19-20/Task1/gruntfile.js
+++ GoIT/JavaScript/lesson19-20/Task1/gruntfile.js
@@ -57,5 +57,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ["concat", 'uglify', 'sass', "cssmin"]);
+  grunt.registerTask('default', ['sass', "concat", 'uglify', "cssmin"]);
 };
