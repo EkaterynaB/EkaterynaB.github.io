67726fdf2ab586df0b9f3b1a7d94c65ef84cfaf2
EkaterynaB <kattyB2701@gmail.com>
2016-06-19 00:53:33 +0300

    corrected

--- GoIT/JavaScript/exam/src/js/js.js
+++ GoIT/JavaScript/exam/src/js/js.js
@@ -3,52 +3,51 @@ $(function() {
     function getImagesByRequest(phrase) {
 
         $.ajax({
-        url: 'http://api.pixplorer.co.uk/image?word='+ phrase +'&amount=7&size=l',
-        type: "GET",
-        dataType: "json",
-        cache: false,
-        success: function(data){
-            $(".grid").remove();
-            var piclist = tmpl($('#activity__template').html(), {data: data});
-            $('.activity .wrapper').append(piclist);
+            url: 'http://api.pixplorer.co.uk/image?word='+ phrase +'&amount=7&size=tb',
+            type: "GET",
+            dataType: "json",
+            cache: false,
+            success: function(data){
 
-            $('.grid').isotope({
-                itemSelector: '.grid-item',
-                layoutMode: 'packery',
-                packery: {
-                    gutter: 0
-                }
-            });
+                $(".grid").remove();
+                var piclist = tmpl($('#activity__template').html(), {data: data.images});
+                $('.activity .wrapper').append(piclist);
 
-        }
+                $('.grid').isotope({
+                    itemSelector: '.grid-item',
+                    layoutMode: 'packery',
+                    packery: {
+                        gutter: 0
+                    }
+                });
+
+            }
+        });
+
+    }
+
+
+    getImagesByRequest();
+
+    var buttonSearch = $('.search__button');
+
+    $('.search__button').click(function(e) {
+        e.preventDefault();
+        var searchField = $(".input__form").val();
+        getImagesByRequest(searchField);
     });
 
-}
+    $('.jcarousel').jcarousel({
+    	animation: 'slow',
+    	wrap: 'circular'
+    });
 
+    $('.jcarousel-prev').jcarouselControl({
+		target: '-=1'
+	});
 
+	$('.jcarousel-next').jcarouselControl({
+		target: '+=1'
+	});
 
-getImagesByRequest();
-var buttonSearch = $('.search__button');
-
-$('.search__button').click(function(e) {
-    e.preventDefault();
-    var searchField = $(".input__form").val();
-    getImagesByRequest(searchField);
-})
-
-            $('.jcarousel').jcarousel({
-    			animation: 'slow',
-    			wrap: 'circular'
-    		})
-
-		    $('.jcarousel-prev')
-                .jcarouselControl({
-				target: '-=1'
-			});
-
-		    $('.jcarousel-next')
-                .jcarouselControl({
-				target: '+=1'
-			});
-
-})
+});
